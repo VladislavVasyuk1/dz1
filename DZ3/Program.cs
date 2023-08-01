6276d7eab7557427268d505730be24073af5a9a9
VLad <hok000hok@gmail.com>
2023-08-01 19:59:50 +0300

    dz4

--- DZ3/Program.cs
+++ DZ3/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+
+
+/* Task 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+double Range(double x1, double x2, double y1, double y2, double z1, double z2)
+{
+    double x = x1 - x2;
+    double y = y1 - y2;
+    double z = z1 - z2;
+    double QuartResul = x*x + y*y + z*z;
+    return Math.Round(Math.Sqrt(QuartResul), 2);
+}
+
+Console.Write("Input x1: ");
+double x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input x2: ");
+double x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input y1: ");
+double y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input y2: ");
+double y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input z1: ");
+double z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input z2: ");
+double z2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Range(x1,x2,y1,y2,z1,z2));
+*/
+/* Task 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void Quart( int arg1)
+{
+    int count = 1;
+        if (arg1 > 0)
+            while (count <= arg1)
+                {
+                Console.WriteLine($"{count} - {Math.Pow(count, 3)}");
+                count++;
+                }
+}
+Console.Write("Input the number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Quart(num);
+*/
--- DZ4/Program.cs
+++ DZ4/Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Task 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int quart(int num, int ct)
+{
+    int res = 1;
+    for(int i = 0; i < ct; i++)
+        res *= num;
+return res; 
+}
+
+Console.Write("Input the number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input the degree: ");
+int ct = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(quart(num, ct));
+*/
+
+/* Task 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int sumNumber(int num)
+{
+    int sum = 0;
+    while (num != 0)
+       {
+        sum += num % 10;
+        num = num / 10;
+       }
+    return sum;
+}
+
+Console.Write("Input the number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(sumNumber(number));
+*/
+
+/* Task 29: Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
+
+int [] myArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write("Input the number in the array: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void vArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+}
+
+Console.Write("Input the size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] newArray = myArray(size);
+vArray(newArray);
+*/

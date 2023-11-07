efb64df7aab0a1a17e40828f99e26cdf08bf3884
VLad <hok000hok@gmail.com>
2023-11-07 16:59:57 +0300

    telo

--- kr01/Program.cs
+++ kr01/Program.cs
@@ -1,2 +1,23 @@
 ﻿string[] array1 = new string[] {"вв", "уццу", "вмы", "44", "ывм", "вы", "3535"};
 string[] array2 = new string[array1.Length];
+
+void sorted(string[] array1, string[] array2)
+{
+    int x = 0;
+    for(int i = 1; i < array1.Length; i++)
+        {
+            if (array1[i].Length <= 3)
+                {
+                    array2[x] = array1[i];
+                    x++;
+                }
+        }
+}
+
+
+
+
+
+
+
+
